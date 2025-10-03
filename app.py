5bb34fea885cf55d90e05fd60d0fd42919cca10d
0285753-user <0285753@up.edu.mx>
2025-10-02 20:33:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ url = 'https://raw.githubusercontent.com/Roby20030202/BI_FINAL_PROJECT/refs/head
 df = pd.read_csv(url,index_col=0)
 
 # definiendo columnas del dataset
-columns = ['address','city','state','latitude','longitude','stars','review_count','is_open'] + COFFEE_COLUMNS
+columns =  COFFEE_COLUMNS
 df = df[columns]
 
 # Aseguramos que el DataFrame principal solo contenga negocios relevantes
