d43c3affe8276def7d3ac6f77a9b73f1850ca563
0285753-user <0285753@up.edu.mx>
2025-10-02 20:31:12 -0600

    Update Header.py

--- Modules/UI/Header.py
+++ Modules/UI/Header.py
@@ -5,7 +5,7 @@ def show_header(text_title: str):
   col1, col2 = st.columns([1, 6])
 
   with col1:
-    st.image("image_bi/UPlogo.jpg", width = 200)
+    st.image("image_bi/on_the_rocks_f.jpg", width = 200)
 
   with col2:
     st.title(text_title)
